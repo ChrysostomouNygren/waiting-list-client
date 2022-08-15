25590ea6f6d29f4afb56a26ae5342edccde6b7ad
Katerina <92430746+ChrysostomouNygren@users.noreply.github.com>
2022-08-15 13:10:31 +0200

    fixed failed

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ function App() {
   // Wishlist: Notifikation
 
   useEffect(() => {
-    socket.current = io(https://listiluring-backend.herokuapp.com/);
+    socket.current = io(`https://listiluring-backend.herokuapp.com/`);
 
     socket.current.on("connect", () => {
       console.log("Connected");
